a077522121c73463c39ebf46f33889cbfd9b3abe
kartaphilos <kartaphilos@gmail.com>
2017-03-12 17:35:52 +0000

    Trying to read 'id' to display animal detail

--- src/app/components/animal/animal-detail/animal-detail.component.ts
+++ src/app/components/animal/animal-detail/animal-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
+
+import { Animal } from './../../../core/models';
+import { AnimalService } from './../../../core/services';
 
 @Component({
   selector: 'app-animal-detail',
@@ -6,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./animal-detail.component.scss']
 })
 export class AnimalDetailComponent implements OnInit {
+  animal: Animal;
 
-  constructor() { }
+  breeds = [ //TODO: Put these in DB
+    { value: 'Luisitano' },
+    { value: 'Arab' },
+    { value: 'Cob' },
+    { value: 'Shire' },
+    { value: 'Thoroughbred' },
+    { value: 'Andalusian' },
+    { value: 'Trakehner' },
+    { value: 'Dutch Warmblood' },
+    { value: 'Glue Factory' },
+  ];
+  genders = [
+    { value: 'Mare' },
+    { value: 'Gelding' },
+    { value: 'Stallion' },
+  ];
+  colours = [
+    { value: 'Bay' },
+    { value: 'Chestnut' },
+    { value: 'Dun' },
+    { value: 'Grey' },
+    { value: 'Palomino' },
+    { value: 'Black' },
+    { value: 'White' },
+  ];
 
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private animalService: AnimalService
+  ) { }
+
+  ngOnInit(): void {
+    console.log('id: ', this.route.params['id']);
+    this.route.params
+      .switchMap((params: Params) => this.animalService.getAnimal(params['id']))
+      .subscribe(animal => this.animal = animal);
   }
+
 
 }

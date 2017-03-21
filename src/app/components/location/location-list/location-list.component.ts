12fa8f3c9d88408f0864ecb9f8fc0a7979dcf84e
kartaphilos <kartaphilos@gmail.com>
2017-03-21 15:37:43 +0000

    Initial stab

--- src/app/components/location/location-list/location-list.component.ts
+++ src/app/components/location/location-list/location-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
+import { Router }                 from '@angular/router';
+import { Logger }                 from 'angular2-logger/core';
+
+import { Location, CoOrdinates } from './../../../core/models';
+import { LocationService } from './../../../core/services';
 
 @Component({
   selector: 'app-location-list',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationListComponent implements OnInit {
 
-  constructor() { }
+  locations: Location[];
+  selectedLocation: Location;
+  selectedDialogOption: string;
+
+  constructor(
+    private _logger: Logger,
+    private router: Router,
+    private locationService: LocationService
+  ) {};
 
   ngOnInit() {
+    this.getLocations();
   }
+
+  getLocations(): void {
+    this.locationService.getLocations()
+      .then(locations => this.locations = locations);
+  }
+
+  onSelect(location: Location): void {
+    this.selectedLocation = location;
+  }
+
+  gotoDetail(location: Location): void {
+    this.router.navigate(['/location/detail', location._id]);
+  }
+
+  add(location: Location): void {
+    if (!location) { return; }
+    this.locationService.create(location)
+      .then(a => {
+        this.locations.push(a);
+        this.selectedLocation = null;
+      });
+  }
+
+  create(): void {
+    this.router.navigate(['/location/detail/']);
+  }
+
 
 }

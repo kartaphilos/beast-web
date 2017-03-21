98408158e27738a348f99b1116e03044e1dedbb7
kartaphilos <kartaphilos@gmail.com>
2017-03-21 14:53:42 +0000

    First pass at Location model

--- src/app/core/models/location.ts
+++ src/app/core/models/location.ts
@@ -1,2 +1,18 @@
-export class Location {
+export interface Location {
+  _id: string;
+  location_since: Date;
+  loctype: 'string';
+  name: 'string';
+  number: 'string';
+  street: 'string';
+  city: 'string';
+  county: 'string';
+  postcode: 'string';
+  country: 'string'
+  coordinates: CoOrdinates;
+}
+
+export interface CoOrdinates {
+  lat: 'string';
+  long: 'string';
 }

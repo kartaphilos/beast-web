5ce837e70c47688671f2301e520fbbe769f057f2
kartaphilos <kartaphilos@gmail.com>
2017-03-21 12:56:29 +0000

    Refactoring tidy up of functions, debug, comments

--- src/app/components/animal/animal-detail/animal-detail.component.ts
+++ src/app/components/animal/animal-detail/animal-detail.component.ts
@@ -6,9 +6,9 @@ import { UUID }                   from 'angular2-uuid';
 import { Location }               from '@angular/common';
 import * as moment                from 'moment';
 import { Logger }                 from 'angular2-logger/core';
-
 import 'rxjs/add/operator/switchMap';
 
+// My Stuff
 import { Animal, Name, Birth, Constants } from './../../../core/models';
 import { AnimalService } from './../../../core/services';
 import { ConstantsService } from './../../../core/services';
@@ -19,13 +19,13 @@ import { ConstantsService } from './../../../core/services';
   styleUrls: ['./animal-detail.component.scss']
 })
 export class AnimalDetailComponent implements OnInit {
-  animal: Animal = <Animal>{ name: {}, birth: {}, species: 'horse' };
   constants: Constants;
-  age: number;
-  private id;
+  animal: Animal = <Animal>{ name: {}, birth: {}, species: 'horse' };
+  private age: number;
+  private id: string;
   private isNewAnimal: boolean = true;
   private isReadOnly: boolean = true;
-  private hideForm: boolean = false; // Not really needed as just playing around.
+  private hideForm: boolean = true;
 
   constructor(
     private _logger: Logger,
@@ -37,92 +37,55 @@ export class AnimalDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.getConstants();
-    // Logic to decide if a 'new'/create animal or if a view/edit of existing animal
-    // by url?/params? => ID=null||new
-    this.getId();
-    //this.getAnimal();
+    this.setNewOrExistingAnimal();
   }
 
   getConstants() {
     this.constantsService.getConstants()
       .subscribe(consts => {
-        this.constants = consts[0];
+        this.constants = consts[0]; // Come back as array
         this._logger.debug('constants: ', this.constants);
         this._logger.debug('constants[0].breeds: ', this.constants.breeds);
       });
   }
 
-  getAnimal(): void {
-    this.route.params
-      .switchMap((p: Params) => this.animalService.getAnimal(p['id']))
-      .subscribe(a => {
-        this.animal = a;
-        this.calculateAge();
-        this._logger.debug('animal: ', this.animal);
-        this._logger.debug('age: ', this.age);
-      });
-  }
-
-  calculateAge(): void {
-    if (this.animal.birth.date) {
-      this.age = Math.floor((Date.now() - new Date(this.animal.birth.date).getTime()) / 31556952000);
-    }
-  }
-
-  calculateBirthDate(yrs: number): string {
-    let now = moment();
-    this._logger.debug('moment: ', now);
-    let birth = moment().subtract(yrs, 'years');
-    this._logger.debug('birth: ', birth.format);
-    return birth.format();
-  }
-
-  getId(): void {
+  setNewOrExistingAnimal(): void {
     this.route.params.subscribe(params => {
       this.id = params['id'];// || null;
       this._logger.debug('id: ', this.id);
-      if (this.id != null) {
+      if (this.id) {
         this.isNewAnimal = false;
         this._logger.debug('New? ', this.isNewAnimal);
         this.getAnimal();
       }
       else {
+        this.isNewAnimal = true;
         this._logger.debug('New? ', this.isNewAnimal);
-        this.newAnimal();
+        this.isReadOnly = false; //No need to readonly view a blank animal
         this._logger.debug('Blank Animal: ', this.animal);
       }
     })
   }
 
-  newAnimal(): void {
-    this._logger.debug('Creating blank animal');
-    //this.animal = new Animal('',null, '', '', '', '', '', null, null, '', '', '', null);
-    this.animal.patient_since = new Date();
-    this.isReadOnly = false;
-
-  }
-
-  cancelEdit() {
-    this.getAnimal();
-    this.toggleReadOnly();
+  getAnimal(): void {  //TODO: add error checking to catch for bad id
+    this.route.params
+      .switchMap((p: Params) => this.animalService.getAnimal(p['id']))
+      .subscribe(a => {
+        this.animal = a;
+        this._logger.debug('animal: ', this.animal);
+        this.calculateAge();
+        this._logger.debug('age: ', this.age);
+      });
   }
 
   saveAnimal({ value, valid }: { value: any, valid: boolean }) {
-    // Read form values
     this._logger.debug('Form Animal: ', value);
-    this._logger.debug('Valid? ', valid);
-    this._logger.debug('Form Activity: ', value.activity);
-    this._logger.debug('Form Age: ', value.age);
     this.animal.birth.date = new Date(this.calculateBirthDate(value.age));
     if (!this.animal.name.full) this.animal.name.full = this.animal.name.display;
-
     if (this.isNewAnimal) {
       this._logger.debug('Create call');
-      // Following needed in dev state for in-memory API
-      this.animal.patient_since = new Date();
+      if (!this.animal.patient_since) this.animal.patient_since = new Date(); // If blank date default to now
       this.animal.id = UUID.UUID();
-      // Set full to display if not set
-
       this._logger.debug('UUID: ', this.animal.id);
       this._logger.debug('New Animal: ', this.animal);
       this.animalService.create(this.animal)
@@ -135,11 +98,26 @@ export class AnimalDetailComponent implements OnInit {
     }
   }
 
-  goBack() {
-    this._logger.debug('TODO Naviagate back');
-    this.location.back();
+  cancelEdit() {
+    this.getAnimal();
+    this.toggleReadOnly();
   }
 
+  calculateAge(): void {
+    if (this.animal.birth.date) {
+      this.age = Math.floor((Date.now() - new Date(this.animal.birth.date).getTime()) / 31556952000);
+    }
+  }
+
+  calculateBirthDate(years: number): string {
+    let now = moment();
+    let birth = moment().subtract(years, 'years');
+    return birth.format();
+  }
+
+  goBack() { this.location.back() }
+
+  // Toggle functions for variables
   toggleReadOnly() { this.isReadOnly = !this.isReadOnly }
   toggleHideForm() { this.hideForm = !this.hideForm }
 

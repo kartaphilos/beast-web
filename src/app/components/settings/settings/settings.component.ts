9960cd1692f45f81955af52303c2cf49e979081f
kartaphilos <kartaphilos@gmail.com>
2017-03-22 14:02:42 +0000

    constructing?

--- src/app/components/settings/settings/settings.component.ts
+++ src/app/components/settings/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }                 from '@angular/router';
+import { Logger }                 from 'angular2-logger/core';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _logger: Logger,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this._logger.log('Settings has arrived!');
   }
 
 }

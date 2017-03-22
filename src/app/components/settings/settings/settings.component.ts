ff5797f8cc9494a747cef8bffa4778318b31cddf
kartaphilos <kartaphilos@gmail.com>
2017-03-22 17:38:22 +0000

    Playing with extracting data from Google auth2

--- src/app/components/settings/settings/settings.component.ts
+++ src/app/components/settings/settings/settings.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router }                 from '@angular/router';
-import { Logger }                 from 'angular2-logger/core';
+import { Component, OnInit }  from '@angular/core';
+import { Router }             from '@angular/router';
+import { Logger }             from 'angular2-logger/core';
+import { Subscription }       from 'rxjs/Subscription';
+
+import { GlobalEventsService } from './../../../core/services/';
+
 
 @Component({
   selector: 'app-settings',
@@ -9,10 +13,25 @@ import { Logger }                 from 'angular2-logger/core';
 })
 export class SettingsComponent implements OnInit {
 
+  subscription: Subscription;
+  googleUser: any;
+  name: string;
+  email: string;
+
   constructor(
+    private eventsService: GlobalEventsService,
     private _logger: Logger,
     private router: Router,
-  ) { }
+  ) {
+    this.subscription = eventsService.userLoggedInEvent$.subscribe(
+      user => {
+        this._logger.log('Settings.component - User Logged In: ', user);
+        this.googleUser = user;
+        this.name = user.getBasicProfile().getName();
+        this._logger.log('SettingsComponent: User Name: ', this.name);
+        
+    });
+  }
 
   ngOnInit() {
     this._logger.log('Settings has arrived!');

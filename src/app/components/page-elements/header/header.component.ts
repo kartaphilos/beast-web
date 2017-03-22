028d719655b383b06caa2fbc70d3f5d0b66d055e
kartaphilos <kartaphilos@gmail.com>
2017-03-22 14:03:23 +0000

    Custom google button login and logout working

--- src/app/components/page-elements/header/header.component.ts
+++ src/app/components/page-elements/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router }                 from '@angular/router';
+import { Logger }                 from 'angular2-logger/core';
 
-declare var gapi:any;
+declare var gapi: any;
 
 @Component({
   selector: 'app-header',
@@ -9,16 +11,59 @@ declare var gapi:any;
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  googleLoginButtonId = "google-login-button";
+  userAuthToken = null;
+  userDisplayName = "empty";
+
+  constructor(
+    private _logger: Logger,
+    private router: Router,
+    private zone: NgZone,
+  ) { }
 
   ngOnInit() {
   }
 
-  signOut(): void {
-      console.log('Signout called');
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then( () => {
-        console.log('User signed out.');
-      });
+  ngAfterViewInit() {
+      gapi.signin2.render(
+        this.googleLoginButtonId,
+        {
+          "onSuccess": this.onGoogleLoginSuccess,
+          "scope": "profile",
+          "theme": "dark"
+        });
     }
+
+  onGoogleLoginSuccess = (loggedInUser) => {
+    this.zone.run(() => {
+      this.userAuthToken = loggedInUser.getAuthResponse().id_token;
+      this.userDisplayName = loggedInUser.getBasicProfile().getName();
+      let profile = loggedInUser.getBasicProfile();
+      this._logger.log('ID: ' + profile.getId());
+      this._logger.log('Name: ' + profile.getName());
+      this._logger.log('Image URL: ' + profile.getImageUrl());
+      this._logger.log('Email: ' + profile.getEmail());
+    });
+  }
+
+  onSignIn(googleUser): void {
+    let profile = googleUser.getBasicProfile();
+    this._logger.log('ID: ' + profile.getId());
+    this._logger.log('Name: ' + profile.getName());
+    this._logger.log('Image URL: ' + profile.getImageUrl());
+    this._logger.log('Email: ' + profile.getEmail());
+  }
+
+  signOut(): void {
+    console.log('Signout called');
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      console.log('User signed out.');
+    });
+  }
+
+  gotoUserSettings(): void {
+    this.router.navigate(['/settings']);
+  }
+
 }

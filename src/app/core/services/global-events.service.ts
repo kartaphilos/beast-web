fd213f36722ea0cdecdb166aca248e23faafefbb
kartaphilos <kartaphilos@gmail.com>
2017-03-22 17:37:38 +0000

    Now using BehaviorSubject

--- src/app/core/services/global-events.service.ts
+++ src/app/core/services/global-events.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject }    from 'rxjs/BehaviorSubject';
+
+declare var gapi: any;
 
 @Injectable()
 export class GlobalEventsService {
 
+  // Observable string sources
+  private userLoggedInSource = new BehaviorSubject<any>("");
+
+  // Observable string streams
+  userLoggedInEvent$ = this.userLoggedInSource.asObservable();
+
   constructor() { }
+
+  // Service message commands
+  eventUserLogin(user: string) {
+    this.userLoggedInSource.next(user);
+  }
+
 
 }
